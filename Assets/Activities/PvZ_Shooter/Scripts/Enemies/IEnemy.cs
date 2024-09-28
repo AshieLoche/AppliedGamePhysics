aa734f6f56bf9f8473c5601ecd9b2e9ec1a259e7
CIITSHSDTP00\CIIT <asherstar1001@gmail.com>
2024-09-28 16:08:34 +0800

    Room Activity 2

--- Assets/Activities/PvZ_Shooter/Scripts/Enemies/IEnemy.cs
+++ Assets/Activities/PvZ_Shooter/Scripts/Enemies/IEnemy.cs
@@ -14,6 +14,6 @@ public interface IEnemy
     float PeripheralRange { get; }
 
     void OnDrawGizmos();
-    void Attak
+    void Attack();
 
 }
--- Assets/Activities/PvZ_Shooter/Scripts/Enemies/Squash.cs
+++ Assets/Activities/PvZ_Shooter/Scripts/Enemies/Squash.cs
@@ -14,14 +14,24 @@ public class Squash : MonoBehaviour, IEnemy
 
     public float PeripheralRange => 100;
 
+    public void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
     public void OnDrawGizmos()
     {
+
+        #region Peripheral Vision
         Vector3 negativePeripheral = (SightRange * Mathf.Tan(PeripheralRange / 2 / 180 * Mathf.PI) * -transform.right) + (SightRange * transform.forward);
         Vector3 positivePerihperal = (SightRange * Mathf.Tan(PeripheralRange / 2 / 180 * Mathf.PI) * transform.right) + (SightRange * transform.forward);
 
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, negativePeripheral);
         Gizmos.DrawLine(transform.position, positivePerihperal);
+        #endregion
+
+        //#region 
     }
 
 }
